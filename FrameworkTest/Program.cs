df1ee5fc72dd04d79c8c29a8e910583a973faa19
PabloSoligo2014 <psoligo@ing.unlam.edu.ar>
2015-02-21 12:15:52 -0300

    Subida inicial de esqueleto de proyecto
    Subida inicial de esqueleto de proyecto

---
+++ FrameworkTest/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace FrameworkTest {
+  class Program {
+    static void Main(string[] args) {
+
+      /*
+      ContextRepository cx = new ContextRepository(Properties.Settings.Default.ConnectionString);
+
+      List<Usuario> us = cx.GetUsuarios();
+
+      foreach(Usuario u in us){
+        Console.WriteLine("Usuario:" + u.Nombre);
+        
+      }
+
+      Console.WriteLine("-----------------------------------");
+
+      Usuario nu = new Usuario();
+      nu.Nombre = "peper";
+      nu.Password = "nada";
+      nu.Login = "peper";
+
+      cx.add(nu);
+
+      foreach (Usuario u in us) {
+        Console.WriteLine("Usuario:" + u.Nombre);
+
+      }
+
+      cx.Save();
+      cx.Delete(nu);
+      cx.Save();
+
+      Console.WriteLine("-----------------------------------");
+      foreach (Usuario u in us) {
+        Console.WriteLine("Usuario:" + u.Nombre);
+
+      }
+
+
+      Console.Read();
+
+
+      */
+
+
+    }
+  }
+}
