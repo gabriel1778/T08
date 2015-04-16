15685593e87395978e268bb33707d71d48d2802b
PabloSoligo2014 <psoligo@ing.unlam.edu.ar>
2015-04-16 19:17:02 -0300

    Clase demo dentro de namespace Repository.Model
    Ejemplo de namespace para los grupos que tengan que desarrollar las
    primera clases.

--- FrameworkTest/Program.cs
+++ FrameworkTest/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Repository.Model;
+
 
 
 
@@ -8,45 +10,16 @@ namespace FrameworkTest {
   class Program {
     static void Main(string[] args) {
 
-      /*
-      ContextRepository cx = new ContextRepository(Properties.Settings.Default.ConnectionString);
 
-      List<Usuario> us = cx.GetUsuarios();
-
-      foreach(Usuario u in us){
-        Console.WriteLine("Usuario:" + u.Nombre);
-        
-      }
-
-      Console.WriteLine("-----------------------------------");
-
-      Usuario nu = new Usuario();
-      nu.Nombre = "peper";
-      nu.Password = "nada";
-      nu.Login = "peper";
-
-      cx.add(nu);
-
-      foreach (Usuario u in us) {
-        Console.WriteLine("Usuario:" + u.Nombre);
-
-      }
-
-      cx.Save();
-      cx.Delete(nu);
-      cx.Save();
-
-      Console.WriteLine("-----------------------------------");
-      foreach (Usuario u in us) {
-        Console.WriteLine("Usuario:" + u.Nombre);
-
-      }
+        Console.Read();
 
 
-      Console.Read();
+        Demo de = new Demo();
+        de.MyProperty1 = 7;
+        Console.WriteLine(de.MyProperty1.ToString());
 
 
-      */
+        Console.ReadKey();
 
 
     }
---
+++ Repository/Model/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Model
+{
+    public class Demo
+    {
+        //Getter Setter demo
+        private int myProperty1;
+
+        public int MyProperty1 { 
+            get {
+                return myProperty1;
+            }
+            
+            
+            set{
+            
+                myProperty1 = value;
+            } 
+        
+        }
+            
+    }
+}
