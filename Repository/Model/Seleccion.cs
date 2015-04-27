797c4e952416e99941bf8ef81ee3acbbc7adb153
juliandel <julian_del@hotmail.com>
2015-04-27 13:59:02 -0300

    Seleccion.cs
    Se agrega la clase Seleccion con los atributos:
    
    idSeleccion de tipo integer.
    Descripcion de tipo string, proteger para que no supere los 255
    caracteres.

---
+++ Repository/Model/Seleccion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Model.Seleccion
+{
+    public class Seleccion
+    {
+        private int idSeleccion = 0;
+        private string descripcion = "Seleccion sin Descripcion valida";
+
+        public Seleccion(int idSeleccion, string descripcion)
+        {
+            this.idSeleccion = idSeleccion;
+            if (descripcion.Length < 256)
+            {
+                this.descripcion = descripcion;
+            }
+        }
+
+        public int IdSeleccion
+        {
+            get
+            {
+                return idSeleccion;
+            }
+            set
+            {
+                idSeleccion = value;
+
+            }
+            
+        }
+        public string Descripcion
+        {
+            get
+            {
+                return descripcion;
+
+            }
+            set
+            {
+                if (value.Length < 256)
+                {
+                    descripcion = value.Substring(0, 255);
+                    
+                }
+                    
+            }
+        
+        }
+    }
+}
